599ae9d2f0aded9c4917c7d87727c9244b929f89
Xianwei Cheng <54332923+XianweiCheng01@users.noreply.github.com>
2023-08-05 15:02:13 -0700

    Add files via upload

---
+++ sst-macro/tests/sumi/domain.cc
@@ -0,0 +1,220 @@
+/**
+Copyright 2009-2022 National Technology and Engineering Solutions of Sandia,
+LLC (NTESS).  Under the terms of Contract DE-NA-0003525, the U.S. Government
+retains certain rights in this software.
+
+Sandia National Laboratories is a multimission laboratory managed and operated
+by National Technology and Engineering Solutions of Sandia, LLC., a wholly
+owned subsidiary of Honeywell International, Inc., for the U.S. Department of
+Energy's National Nuclear Security Administration under contract DE-NA0003525.
+
+Copyright (c) 2009-2022, NTESS
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+
+    * Neither the name of the copyright holder nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+Questions? Contact sst-macro-help@sandia.gov
+*/
+
+#include <sprockit/test/test.h>
+#include <sprockit/output.h>
+#include <sstmac/util.h>
+#include <sstmac/compute.h>
+#include <sstmac/software/process/app.h>
+#include <sstmac/software/process/operating_system.h>
+#include <sstmac/software/process/thread.h>
+#include <sstmac/skeleton.h>
+#include <sumi/dense_rank_map.h>
+#include <sumi/transport.h>
+#include <sumi/sumi.h>
+
+#define sstmac_app_name user_app_cxx
+using namespace sumi;
+
+void
+run_test(Communicator* dom, int todie, int nproc_live, int context, int tag)
+{
+  int me = comm_rank();
+  auto known_failures = comm_failed_ranks(context);
+  DenseRankMap rmap(known_failures);
+  int dense_me = rmap.denseRank(me);
+  if (me == 11){
+    printf("Rank 11 maps to dense rank %d with failures = %s\n",
+      dense_me, known_failures.toString().c_str());
+  }
+
+  CollectiveDoneMessage* dmsg;
+
+  int src[] = { dense_me };
+  int dst[nproc_live];
+  comm_allgather(dst, src, 1, sizeof(int), tag, true, context, dom);
+
+  dmsg = comm_collective_block(Collective::allgather, tag);
+  if (!dmsg->succeeded()){
+    spkt_throw_printf(sprockit::IllformedError,
+        "allgather collective failed with failures %s, should always succeed",
+        dmsg->failed_procs().toString().c_str());
+  }
+
+  DenseRankMap domain_rmap(known_failures, dom);
+  for (int i=0; i < nproc_live; ++i){
+    int sparse_rank = domain_rmap.sparseRank(i);
+    int global_rank = dom->commToGlobalRank(sparse_rank);
+    int correct = rmap.denseRank(global_rank);
+    if (dst[i] != correct){
+      for (int j=0; j < nproc_live; ++j){
+        std::cerr << sprockit::sprintf("A[%d] = %d\n", j, dst[j]);
+      }
+      spkt_throw_printf(sprockit::ValueError,
+        "Rank %d A[%d] = %d != %d for global rank %d, sparse rank %d on test nproc=%d,tag=%d",
+        me, i, dst[i], correct, global_rank, sparse_rank, nproc_live, tag);
+    }
+  }
+  printf("Rank %d passed allgather\n", me);
+
+  sstmac_usleep(100);
+}
+
+void
+test_allreduce(Communicator* dom, int tag)
+{
+  //now do a collective with payloads
+  int rank = dom->myCommRank();
+  int nproc = dom->nproc();
+  int nelems = 2*nproc;
+  int numfill = 2*rank + 1;
+  int* src_buffer = new int[nelems];
+  ::memset(src_buffer, 0, nelems * sizeof(int));
+  for (int i=0; i <= numfill; ++i){
+    src_buffer[i] = 1;
+  }
+  int* dst_buffer = new int[nelems];
+  comm_allreduce<int,Add>(dst_buffer, src_buffer, nelems, tag, false, options::initial_context, dom);
+
+  Message* msg = comm_poll(); //wait on allreduce
+  if (msg->classType() != Message::collective_done){
+    spkt_throw_printf(sprockit::ValueError,
+      "allreduce test: expected collective message, but got %s",
+      Message::tostr(msg->classType()));
+  }
+
+  if (rank == 0){
+    printf("Testing allreduce with payload\n");
+    for (int i=0; i < nelems; ++i){
+      printf("test[%d] = %d\n", i, dst_buffer[i]);
+    }
+  }
+}
+
+void
+test_allgather(Communicator* dom, int tag)
+{
+  int nelems = 10;
+
+  int rank = dom->myCommRank();
+  int nproc = dom->nproc();
+
+  int* src_buffer = new int[nelems];
+  for (int i=0; i < nelems; ++i){
+    src_buffer[i] = rank;
+  }
+
+
+
+  int* dst_buffer = new int[nproc*nelems];
+
+  comm_allgather(dst_buffer, src_buffer, nelems, sizeof(int), tag, false, options::initial_context, dom);
+
+  Message* msg = comm_poll(); //wait on allgather
+  if (msg->classType() != Message::collective_done){
+    spkt_throw_printf(sprockit::ValueError,
+      "allreduce test: expected collective message, but got %s",
+      Message::tostr(msg->classType()));
+  }
+
+  if (rank == 0){
+    std::cout << "Testing allgather payload with " << nelems << " elements\n";
+  }
+
+  int* bufptr = dst_buffer;
+  int idx = 0;
+  for (int p=0; p < nproc; ++p){
+    for (int i=0; i < nelems; ++i, ++bufptr, ++idx){
+      int test_elem = *bufptr;
+      if (test_elem != p){
+        std::cout << sprockit::sprintf("FAILED: allgather rank %d, section %d\n",
+            rank, p);
+      }
+      //std::cout << sprockit::spktprintf("T[%d][%d] = %d\n", rank, idx, test_elem);
+    }
+  }
+
+}
+
+
+
+
+
+
+int
+main(int argc, char **argv)
+{
+  comm_init();
+
+  //now do a collective with payloads
+  int rank = comm_rank();
+  int nproc = comm_nproc();
+
+  int start = 2, nsubrange = 4;
+  int stop = start + nsubrange;
+
+  if (rank >= start && rank < stop){
+    Communicator* dom = new SubrangeCommunicator(rank, start, nsubrange);
+    //test_allgather(dom, 0);
+    //test_allreduce(dom, 1);
+  }
+
+  Communicator* dom = new RotateCommunicator(rank, nproc, 3);
+  //test_allgather(dom, 2);
+  //test_allreduce(dom, 3);
+
+
+  run_test(dom, 1, 12, options::initial_context, 4);
+
+  run_test(dom, 4, 11, 4, 5);
+
+  run_test(dom, 7, 10, 5, 6);
+
+  run_test(dom, 10, 9, 6, 7);
+
+  comm_finalize();
+
+  return 0;
+}
+
---
+++ sst-macro/tests/sumi/saturation.cc
@@ -0,0 +1,238 @@
+/**
+Copyright 2009-2022 National Technology and Engineering Solutions of Sandia,
+LLC (NTESS).  Under the terms of Contract DE-NA-0003525, the U.S. Government
+retains certain rights in this software.
+
+Sandia National Laboratories is a multimission laboratory managed and operated
+by National Technology and Engineering Solutions of Sandia, LLC., a wholly
+owned subsidiary of Honeywell International, Inc., for the U.S. Department of
+Energy's National Nuclear Security Administration under contract DE-NA0003525.
+
+Copyright (c) 2009-2022, NTESS
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+
+    * Neither the name of the copyright holder nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+Questions? Contact sst-macro-help@sandia.gov
+*/
+
+#include <sprockit/test/test.h>
+#include <sprockit/util.h>
+#include <sprockit/sim_parameters.h>
+#include <sprockit/keyword_registration.h>
+#include <sstmac/common/sstmac_env.h>
+#include <sstmac/software/process/app.h>
+#include <sstmac/software/process/operating_system.h>
+#include <sstmac/hardware/topology/traffic/traffic.h>
+#include <sstmac/hardware/topology/topology.h>
+#include <sstmac/skeleton.h>
+#include <sumi/transport.h>
+#include <sumi/sumi.h>
+#include <sumi/sumi_thread.h>
+
+#define sstmac_app_name user_app_cxx
+using namespace sumi;
+using sstmac::TimeDelta;
+using sstmac::hw::traffic_pattern;
+using sstmac::NodeId;
+using sstmac::Env;
+using sstmac::hw::Topology;
+
+RegisterKeywords(
+"traffic_pattern",
+"inject_time",
+"network_injector_capacity_bw"
+);
+
+//static long done = 0;
+
+static int num_finished = 0;
+static double average_latency_ms = 0;
+static long num_messages_counted = 0;
+//static long short_msg_length = 8000;
+static double latency_total = 0;
+typedef std::unordered_map<sumi::Message*,double> time_map;
+typedef std::unordered_map<int, time_map> rank_time_map;
+static rank_time_map start_times;
+
+
+class throughput_thread :
+  public sstmac::SumiThread
+{
+ public:
+  throughput_thread(sstmac::sw::SoftwareId sid) :
+    sstmac::SumiThread(sid){}
+
+  virtual void run();
+
+  ~throughput_thread(){}
+};
+
+void
+throughput_thread::run()
+{
+  //int num_recved = 0;
+  while (1) {
+    Message* msg = comm_poll();
+    if (msg->payload_type() == Message::rdma_put_ack) {
+      //ignore
+    } else if (msg->payload_type() == Message::rdma_put) {
+      time_map& times = start_times[msg->sender()];
+      time_map::iterator it = times.find(msg.get());
+      if (it == times.end()) {
+        continue; //ignore the first message
+      }
+
+      double start_time = it->second;
+      times.erase(it);
+      double now = wall_time();
+      double delta = now - start_time;
+      double delta_ms = delta * 1e3;
+      latency_total += delta_ms;
+      num_messages_counted++;
+      average_latency_ms = latency_total / num_messages_counted;
+      return;
+    } else {
+      spkt_throw_printf(sprockit::IllformedError,
+                       "got unexpected message %s",
+                       sumi::Message::tostr(msg->payload_type()));
+    }
+  }
+
+}
+
+void run_test(
+  traffic_pattern::type_t ty,
+  long inject_length,
+  double offered_load_bw
+)
+{
+  std::vector<NodeId> node_partners;
+  sstmac::sumi_api* simp = safe_cast(sstmac::sumi_api, sumi_api());
+  Topology::global()->send_partners(
+    ty,
+    simp->my_addr(),
+    node_partners);
+  int num_partners = node_partners.size();
+  std::vector<int> send_partners(num_partners, 0);
+  for (size_t i=0; i < node_partners.size(); ++i) {
+    send_partners[i] = simp->get_partner(node_partners[i]);
+  }
+
+  node_partners.clear();
+  std::vector<int> recv_partners(num_partners, 0);
+  Topology::global()->recv_partners(
+    ty,
+    simp->my_addr(),
+    node_partners);
+  for (int i=0; i < num_partners; ++i) {
+    recv_partners[i] = simp->get_partner(node_partners[i]);
+  }
+
+  int aid = 1; //assume 1 for now
+  sstmac::sw::SoftwareId sid(aid, comm_rank());
+  throughput_thread* thr = new throughput_thread(sid);
+  thr->start();
+
+  int me = comm_rank();
+  for (int i=0; i < num_partners; ++i) {
+    sumi::Message* msg = new sumi::Message(inject_length);
+    comm_rdma_put(send_partners[i], msg);
+
+    // sleep until the message WOULD be done
+    // injecting in the absence of congestion
+    double delay = inject_length / offered_load_bw;
+    sleep(delay);
+
+    //now send a single, small message
+    msg = new sumi::Message(8000);
+    comm_rdma_put(send_partners[i], msg);
+    start_times[me][msg.get()] = wall_time();
+  }
+
+  thr->join();
+}
+
+int
+main(int argc, char** argv)
+{
+  comm_init();
+
+  SST::Params& params = sstmac::sw::App::getParams();
+
+  std::string pattern = params.find<std::string>("traffic_pattern");
+  traffic_pattern::type_t ty;
+  if (pattern == "NN" || pattern == "nearest_neighbor") {
+    ty = traffic_pattern::nearest_neighbor;
+  } else if (pattern == "BC" || pattern == "bit_complement") {
+    ty = traffic_pattern::bit_complement;
+  } else if (pattern == "TOR" || pattern == "tornado") {
+    ty = traffic_pattern::tornado;
+  } else {
+    spkt_throw_printf(sprockit::InputError,
+                     "invalid traffic pattern %s",
+                     pattern.c_str());
+  }
+
+  double offered_load_bw = 0;
+
+  if (params.contains("pisces_injection_bandwidth")) {
+    offered_load_bw = params.find<SST::UnitAlgebra>("pisces_injection_bandwidth").getValue().toDouble();
+  } else if (params.contains("cycle_accurate_switch_bandwidth_n2r")) {
+    offered_load_bw = params.find<SST::UnitAlgebra>("cycle_accurate_switch_bandwidth_n2r").getValue().toDouble();
+  } else if (params.contains("network_injector_capacity_bw")) {
+    offered_load_bw = params.find<SST::UnitAlgebra>("network_injector_capacity_bw").getValue().toDouble();
+  } else if (params.contains("packet_switch_bandwidth_n2r")) {
+    offered_load_bw = params.find<SST::UnitAlgebra>("packet_switch_bandwidth_n2r").getValue().toDouble();
+  } else if (params.contains("network_train_injection_bw")) {
+    offered_load_bw = params.find<SST::UnitAlgebra>("network_train_injection_bw").getValue().toDouble();
+  } else {
+    spkt_throw_printf(sprockit::InputError,
+                     "throughput application did not find injection bandwidth");
+  }
+
+  TimeDelta inject_time = params.find<SST::UnitAlgebra>("inject_time");
+  long inject_length = offered_load_bw * inject_time.sec();
+
+  run_test(ty, inject_length, offered_load_bw);
+
+  //int rank = comm_rank();
+  int nproc = comm_nproc();
+
+  comm_finalize();
+
+  ++num_finished;
+
+  if (num_finished == nproc) {
+    std::cout << sprockit::sprintf("Average latency: %10.5f ms\n", average_latency_ms);
+  }
+  return 0;
+}
+
+
